2d40f26a3da595de59848f474cafb050b57289b1
Zhunisali <zshanabek@gmail.com>
2017-12-15 19:33:33 +0600

    [Add] - inline keyboard button

--- bot/coin_bot.py
+++ bot/coin_bot.py
@@ -1,10 +1,25 @@
 import config
 import telebot
-
+from telebot import types
 bot = telebot.TeleBot(config.token)
 
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    welcome_msg = "Здравствуйте {0}. Что вы хотите? Купить или продать?".format(message.chat.first_name)
+    keyboard = types.InlineKeyboardMarkup(row_width = 2)
+    callback_bt1 = types.InlineKeyboardButton(text="Купить", callback_data="1")
+    callback_bt2 = types.InlineKeyboardButton(text="Продать", callback_data="2")
+    keyboard.add(callback_bt1, callback_bt2)
+    bot.send_message(message.chat.id, welcome_msg,reply_markup=keyboard)
+    
+
+
+@bot.message_handler(commands=['help'])
+def send_welcome(message):
+	bot.reply_to(message, "Howdy, how are you doing?")
+
 @bot.message_handler(content_types=["text"])
-def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
+def repeat_all_messages(message):
     bot.send_message(message.chat.id, message.text)
 
 if __name__ == '__main__':
