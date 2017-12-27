829eab2babc739cb39a0568705ef11e102f8c49e
Zhunisali <zshanabek@gmail.com>
2017-12-27 17:58:22 +0600

    [Fix]

--- tasks.py
+++ tasks.py
@@ -1,9 +1,9 @@
 from celery import Celery
 
-app = Celery('tasks', broker='amqp://')
-@app.task 
-def reverse(string):
-    return string[::-1]
+app = Celery('tasks', broker='redis://')
+# @app.task 
+# def reverse(string):
+#     return string[::-1]
 @app.task 
 def add(x, y):
     return x + y
