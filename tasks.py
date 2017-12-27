e0e9ecf46c0984596f5e306891319c8ca0d0ccc9
Zhunisali <zshanabek@gmail.com>
2017-12-27 18:09:49 +0600

    fix

--- tasks.py
+++ tasks.py
@@ -1,6 +1,6 @@
 from celery import Celery
 
-app = Celery('tasks', broker='redis://')
+app = Celery('tasks', broker='pyamqp://localhost/')
 # @app.task 
 # def reverse(string):
 #     return string[::-1]
